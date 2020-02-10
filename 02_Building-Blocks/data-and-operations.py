7c937f130c970d757038c30a21aadaadb581ec89
Angel Plascencia <11001861+aplascencia0188@users.noreply.github.com>
2020-02-10 07:48:04 -0600

    Add Data and Operations lesson

---
+++ 02_Building-Blocks/data-and-operations.py
@@ -0,0 +1,103 @@
+# Operations
+
+# Numbers
+5
+10
+3.1416
+-1001.2
+
+
+##########
+# Strings
+##########
+'Hello, World'
+"Hello, World"
+
+'Single quote, or double quote"
+
+# EOL = End Of Line
+
+'Notice an error without quote
+
+'''I can have
+more than one line'''
+
+print('I can have\nmore than one line')
+
+"""Now I can write a poem
+one more than one like.
+This poem sucks. 
+Better keep my day job..."""
+
+'My dog's name is Domino"
+
+"My dog's name is Domino"
+'''My cat's name is Eskay'''
+
+print("a newline\nlooks like that")
+
+print("A single\\backslash")
+
+print("c:\windows")
+
+print("c:\temp") # it recognize it like tab
+
+########
+# Lists
+########
+[1, 2, 3, 4, 5]
+["milk", "eggs", "butter", "flour", "syrup"]
+[1,2,3,4,5]
+
+
+groceries = ["milk", "eggs", "butter", "flour", "syrup"]
+
+2cool4school = "Nope!"
+cool_4_school = "You too!"
+watch_your_S#dr = 5
+
+################
+# Instructor 
+################
+str()
+str(5)
+
+print("I have " + 5 " apples.")
+print("I have " + str(5) + " apples.")
+
+int()
+
+float()
+
+float(132)
+
+int(3.1416)
+
+list()
+
+list("Hello, my dear!")
+
+# Error
+list(5)
+
+###########
+# Operator
+###########
+5 + 3
+8 - 2
+5 * 3
+-44 / 12
+3 ** 5
+( 2 + 8 ) / 4
+2 + 8 / 4
+
+greeting = "Hello, "
+name = "Angel"
+greeting + name
+
+"na " * 5 + "Batman"
+[ "na" ] * 5 + [ "Batman" ] 
+
+
+
+
